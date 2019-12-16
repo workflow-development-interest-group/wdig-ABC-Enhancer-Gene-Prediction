64247cce556031cf88c6f2941e1b1fb8f70da69b
jnasser <jnasser@broadinstitute.org>
2019-12-16 11:19:58 -0500

    add binning for bedpe powerlaw fit

--- src/compute_powerlaw_fit_from_hic.py
+++ src/compute_powerlaw_fit_from_hic.py
@@ -23,7 +23,7 @@ def parseargs():
     parser.add_argument('--hicDir', help="Directory containing observed HiC KR normalized matrices. File naming and structure should be: hicDir/chr*/chr*.KRobserved")
     parser.add_argument('--outDir', help="Output directory")
     parser.add_argument('--hic_type', help="Output directory")
-    parser.add_argument('--resolution', default=5000, type=int, help="Resolution of hic dataset (in bp)")
+    parser.add_argument('--resolution', default=5000, type=int, help="For Juicebox: resolution of hic dataset (in bp). For bedpe: distances will be binned to this resolution for powerlaw fit")
     parser.add_argument('--minWindow', default=5000, type=int, help="Minimum distance between bins to include in powerlaw fit (bp). Recommended to be at least >= resolution to avoid using the diagonal of the HiC Matrix")
     parser.add_argument('--maxWindow', default=1000000, type=int, help="Maximum distance between bins to include in powerlaw fit (bp)")
     parser.add_argument('--chr', default='all', help="Comma delimited list of chromosomes to use for fit. Defualts to chr[1..22],chrX")
@@ -36,13 +36,6 @@ def main():
     os.makedirs(args.outDir, exist_ok=True)
 
     HiC = load_hic_for_powerlaw(args)
-    # #Juicebox format
-    # if args.hic_type == 'juicebox':
-    #     HiC = load_hic_for_powerlaw(args)
-    # elif args.hic_type == 'bedpe':
-    #     HiC = load_hic_for_powerlaw(args)
-    # else:
-    #     error('must set --hic_type')
 
     #Run 
     slope, intercept, hic_mean_var = do_powerlaw_fit(HiC)
@@ -90,8 +83,12 @@ def load_hic_for_powerlaw(args):
                                      min_window = None, 
                                      gamma = None)
 
-                this_data['dist_for_fit'] = abs((this_data['x2'] + this_data['x1'])/2 - (this_data['y2'] + this_data['y1'])/2)
-                this_data = this_data.loc[np.logical_and(this_data['dist_for_fit'] > args.minWindow, this_data['dist_for_fit'] < args.maxWindow)]
+                #Compute distance in bins as with juicebox data. 
+                #This is needed to in order to maintain consistency, but is probably slightly less accurate.
+                #Binning also reduces noise level.
+                rawdist = abs((this_data['x2'] + this_data['x1'])/2 - (this_data['y2'] + this_data['y1'])/2)
+                this_data['dist_for_fit'] = (rawdist // args.resolution) * args.resolution
+                this_data = this_data.loc[np.logical_and(this_data['dist_for_fit'] >= args.minWindow, this_data['dist_for_fit'] <= args.maxWindow)]
                 all_data_list.append(this_data)
             else:
                 error('invalid --hic_type')
@@ -104,32 +101,8 @@ def load_hic_for_powerlaw(args):
 
     return(all_data)
 
-# def load_hic_bedpe():
-#     if args.chr == 'all':
-#         chromosomes = ['chr' + str(x) for x in  list(range(1,23))] + ['chrX']
-#     else:
-#         chromosomes = args.chr.split(',')
-
-#     all_data_list = []
-#     for chrom in chromosomes:
-#         try:
-#             hic_file, hic_norm_file, hic_is_vc = get_hic_file(chrom, args.hicDir, allow_vc=False, hic_type='juicebox')
-#             print("Working on {}".format(hic_file))
-
-#             this_data = load_hic(hic_file = hic_file,
-#                                  hic_type = 'bedpe')
-
-#             this_data['dist_for_fit'] = abs((this_data['x2'] + this_data['x1'])/2 - (this_data['y2'] + this_data['y1'])/2)
-#             all_data_list.append(this_data)
-#         except Exception as e:
-#             print(e)
-#             traceback.print_exc(file=sys.stdout)
-
 def do_powerlaw_fit(HiC):
     print("Running regression")
-
-    import pdb
-    pdb.set_trace()
 
     #TO DO:
     #Print out mean/var plot of powerlaw relationship
