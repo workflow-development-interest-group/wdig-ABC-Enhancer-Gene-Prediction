b47168fbec170cc5fedfe7744269e3203c113224
kmualim <kristy.mualim@mail.mcgill.ca>
2021-02-27 00:24:03 -0800

    get prediction files for variant overlap

--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,7 @@
 import argparse
 from predictor import *
 from tools import *
+from metrics import *
 import pandas as pd
 import numpy as np
 import sys, traceback, os, os.path
@@ -22,6 +23,7 @@ def get_model_argument_parser():
     parser.add_argument('--score_column', default='ABC.Score', help="Column name of score to use for thresholding")
     parser.add_argument('--threshold', type=float, required=True, default=.022, help="Threshold on ABC Score (--score_column) to call a predicted positive")
     parser.add_argument('--cellType', help="Name of cell type")
+    parser.add_argument('--chrom_sizes', help="Chromosome sizes file")
 
     #hic
     #To do: validate params
@@ -50,6 +52,7 @@ def get_model_argument_parser():
     parser.add_argument('--tss_slop', type=int, default=500, help="Distance from tss to search for self-promoters")
     parser.add_argument('--chromosomes', default="all", help="chromosomes to make predictions for. Defaults to intersection of all chromosomes in --genes and --enhancers")
     parser.add_argument('--include_chrY', '-y', action='store_true', help="Make predictions on Y chromosome")
+    parser.add_argument('--run_metrics', type=bool, default=True, help="Run Metrics")
 
     return parser
 
@@ -72,14 +75,14 @@ def main():
     print("reading genes")
     genes = pd.read_csv(args.genes, sep = "\t")
     genes = determine_expressed_genes(genes, args.expression_cutoff, args.promoter_activity_quantile_cutoff)
-    genes = genes.loc[:,['chr','symbol','tss','Expression','PromoterActivityQuantile','isExpressed']]
-    genes.columns = ['chr','TargetGene', 'TargetGeneTSS', 'TargetGeneExpression', 'TargetGenePromoterActivityQuantile','TargetGeneIsExpressed']
+    genes = genes.loc[:,['chr','symbol','tss','Expression','PromoterActivityQuantile','isExpressed', 'DHS.RPM', 'DHS.RPM.quantile', 'H3K27ac.RPM', 'H3K27ac.RPM.quantile','H3K27ac.RPM.TSS1Kb', 'H3K27ac.RPM.quantile.TSS1Kb','DHS.RPM.TSS1Kb', 'DHS.RPM.quantile.TSS1Kb']]
+    genes.columns = ['chr','TargetGene', 'TargetGeneTSS', 'TargetGeneExpression', 'TargetGenePromoterActivityQuantile','TargetGeneIsExpressed', 'Gene.DHS.RPM', 'Gene.DHS.RPM.quantile', 'Gene.H3K27ac.RPM', 'Gene.H3K27ac.RPM.quantile','Gene.H3K27ac.RPM.TSS1Kb', 'Gene.H3K27ac.RPM.quantile.TSS1Kb','Gene.DHS.RPM.TSS1Kb', 'Gene.DHS.RPM.quantile.TSS1Kb']
        
     print("reading enhancers")
     enhancers_full = pd.read_csv(args.enhancers, sep = "\t")
     #TO DO
     #Think about which columns to include
-    enhancers = enhancers_full.loc[:,['chr','start','end','name','class','activity_base']]
+    enhancers = enhancers_full.loc[:,['chr','start','end','name','class','normalized_dhs', 'normalized_h3K27ac','activity_base','DHS.RPM' ,  'DHS.RPM.quantile', 'H3K27ac.RPM', 'H3K27ac.RPM.quantile']]
 
     #Initialize Prediction files
     pred_file_full = os.path.join(args.outdir, "EnhancerPredictionsFull.txt")
@@ -87,6 +90,7 @@ def main():
     pred_file_bedpe = os.path.join(args.outdir, "EnhancerPredictions.bedpe")
     all_pred_file_expressed = os.path.join(args.outdir, "EnhancerPredictionsAllPutative.txt.gz")
     all_pred_file_nonexpressed = os.path.join(args.outdir, "EnhancerPredictionsAllPutativeNonExpressedGenes.txt.gz")
+    variant_overlap_file = os.path.join(args.outdir, "EnhancerPredictionsAllPutative.ForVariantOverlap.shrunk150bp.txt.gz")
     all_putative_list = []
 
     #Make predictions
@@ -94,6 +98,7 @@ def main():
         chromosomes = set(genes['chr']).intersection(set(enhancers['chr'])) 
         if not args.include_chrY:
             chromosomes.discard('chrY')
+            chromosomes.discard('chr9')
     else:
         chromosomes = args.chromosomes.split(",")
 
@@ -122,9 +127,12 @@ def main():
     all_positive.to_csv(pred_file_full, sep="\t", index=False, header=True, float_format="%.6f")
     all_positive[slim_cols].to_csv(pred_file_slim, sep="\t", index=False, header=True, float_format="%.6f")
 
+    if args.run_metrics:
+        GrabQCMetrics(all_positive, args.outdir)
+    
     make_gene_prediction_stats(all_putative, args)
     write_connections_bedpe_format(all_positive, pred_file_bedpe, args.score_column)
-
+    
     if args.make_all_putative:
         if not args.use_hdf5:
             all_putative.loc[all_putative.TargetGeneIsExpressed,:].to_csv(all_pred_file_expressed, sep="\t", index=False, header=True, compression="gzip", float_format="%.6f", na_rep="NaN")
@@ -134,7 +142,9 @@ def main():
             all_pred_file_nonexpressed = os.path.join(args.outdir, "EnhancerPredictionsAllPutativeNonExpressedGenes.h5")
             all_putative.loc[all_putative.TargetGeneIsExpressed,:].to_hdf(all_pred_file_expressed, key='predictions', complevel=9, mode='w')
             all_putative.loc[~all_putative.TargetGeneIsExpressed,:].to_hdf(all_pred_file_nonexpressed, key='predictions', complevel=9, mode='w')
-            
+   
+    test_variant_overlap(args, all_putative)
+
     print("Done.")
     
 def validate_args(args):
